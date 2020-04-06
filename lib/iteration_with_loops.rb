dcfae686dac79670272c639c93a294a076a3c132
tiya adugna <tiyakefalee@gmail.com>
2020-04-06 07:31:34 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,4 +3,17 @@ def join_nested_strings(src)
   # Combine all Strings present in the AoA into a single value and return it
   str = ""
   count = 0 
+  while str.length > count do
+    num = 0 
+    while str[count].length > num do 
+      
+      if !(str[count][num] % 2)
+        str += " " + str[count][num]
+      end
+      num += 1 
+    end
+    count += 1 
+  end 
+  
+        
 end
